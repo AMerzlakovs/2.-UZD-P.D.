832ff2d91166362650c48c398281a345d43084a8
AleksandrsMerzl <95225110+AMerzlakovs@users.noreply.github.com>
2021-12-15 13:57:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# 2. uzd
+print ("Pirmo ievadi mazāko skaitli! ")
+a = int(input("Ievadi veselo skaitli: "))
+b = int(input("Ievadi veselo skaitli: "))
+print(a)
+for i in range(a, b):
+  print(i + 1)
